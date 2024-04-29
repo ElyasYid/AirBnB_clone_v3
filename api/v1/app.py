1cdc8415755e2c429f72660629450be83184eb87
ElyasYid <elyassayle27@gmail.com>
2024-04-29 04:04:40 -0700

    aaa

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-"""This is the start of RESTful api"""
+"""
+The RESTful api starts here. The api aids data access in the app.
+"""
+from os import getenv
 
 from flask import Flask, jsonify
 from flask_cors import CORS
+
 from api.v1.views import app_views
 from models import storage
-import os
-
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.register_blueprint(app_views, url_prefix="/api/v1")
-CORS(app, origins=["0.0.0.0"]
+app.register_blueprint(app_views)
+CORS(app, origins=["0.0.0.0"])
+host = getenv("HBNB_API_HOST", "0.0.0.0")
+port = getenv("HBNB_API_PORT", "5000")
+
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """the cleanup operation"""
+def teardown(exception):
+    """Cleanup operations"""
     storage.close()
 
 
 @app.errorhandler(404)
-def errorhandler(error):
-    """this is the handler for the 404 errors"""
+def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host, port, threaded=True, debug=True)
